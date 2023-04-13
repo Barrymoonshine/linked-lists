bf7bde107d23b18307e66e78ec10c878c892fc3e
Nick Sheward <nickshwrd@gmail.com>
2023-04-13 14:04:24 +0100

    Updated name of append function to prepend!

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const LinkedList = (head = null) => ({ head });
 
-const NewNode = (value = null, nextNode = null) => ({ value, nextNode });
+const NodeFactory = (value, nextNode = null) => ({ value, nextNode });
 
-console.log(LinkedList());
+const myList = LinkedList();
 
-console.log(NewNode());
+console.log(myList);
+
+const prepend = (value) => {
+  const newNode = NodeFactory(value);
+  // List empty
+  if (myList.head === null) {
+    myList.head = newNode;
+    // List not empty
+  } else if (myList.head !== null) {
+    newNode.nextNode = myList.head;
+    myList.head = newNode;
+  }
+};
+
+const node1 = prepend(1);
+const node2 = prepend(2);
+
+// console.log(node1);
+// console.log(node2);
+console.log(myList);
